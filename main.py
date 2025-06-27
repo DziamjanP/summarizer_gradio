d0cb9304c37657914bf7cfd77264d4f517336511
DziamjanP <demyan.postnikov@gmail.com>
2025-06-27 16:41:23 +0300

    make first gradio page

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
-from together import Together
-from dotenv import dotenv_values
+from summary import summarize
+import gradio as gr
 
-client = Together(api_key=dotenv_values(".env")['TOGETHER_API_KEY'])
+demo = gr.Blocks()
 
-stream = client.chat.completions.create(
-  model="deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free", #later add selection or test other models
-  messages=[
-        {
-          "role": "user",
-          "content": [
-                {
-                    "type": "text",
-                    "text": open('prompt.txt').read(),
-                },
-                {
-                    "type": "text",
-                    "text": open('texts/1.txt').read(),
-                },
-              ]
-        }
-    ],
-  stream=True,
-)
-#make file empty if exists
-f = open('sums/1.txt', 'w+')
-f = open('sums/1.txt', 'a')
+with demo:
+    gr.Markdown(
+    """
+    # Summarizer
+    Paste your text below to get a brief summary
+    """)
+    
+    inp = gr.Textbox(label="Input", placeholder="Your text")
+    submit = gr.Button("Summarize!")
+    out = gr.Textbox(label="Summary")
+    submit.click(summarize, inp, out)
 
-for chunk in stream:
-    f.write(chunk.choices[0].delta.content)
-    print(chunk.choices[0].delta.content or "", end="", flush=True)
-
-#for prettier output
-print()
-
-f.close()
+demo.launch()
