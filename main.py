8bd6a653ca40bf577304613e645943257d6f5d3c
DziamjanP <demyan.postnikov@gmail.com>
2025-06-28 00:11:27 +0300

    two column layout

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ with demo:
     # Summarizer
     Paste your text below to get a brief summary
     """)
-    inp = gr.Textbox(label="Input", placeholder="Your text")
-    upload = gr.File(label="File with text", file_types=[".pdf"])
-    submit = gr.Button("Summarize!")
-    out = gr.Textbox(label="Summary")
+    with gr.Row(equal_height=True):
+      with gr.Column():
+        inp = gr.Textbox(label="Input", placeholder="Your text")
+        upload = gr.File(label="File with text", file_types=[".pdf"])
+        submit = gr.Button("Summarize!")
+      with gr.Column():
+        out = gr.Textbox(label="Summary")
     submit.click(summarize, inp, out)
     upload.upload(parse_pdf, inputs=upload, outputs=inp)
 
