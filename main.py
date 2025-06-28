4036524b06ce18e78ef0de8bbd33afb37d13d2f4
DziamjanP <demyan.postnikov@gmail.com>
2025-06-28 23:05:43 +0300

    add mistral api functions add model selection

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ import gradio as gr
 
 demo = gr.Blocks(title="Summary maker", fill_height=True)
 
-def make_summary(input):
-  output = summarize(input)
+models = {
+  "Mistral Medium": "mistral-medium-2505",
+  "Magistral Medium": "magistral-medium-2506",
+  "Llama 3.3 70B": "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
+  "Deepseek R1 Distill Llama 70B": "deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free",
+}
+
+def make_summary(input, model):
+  output = summarize(input, models[model])
   return [gr.update(value=output), gr.update(value=output)]
     
 with demo:
@@ -18,13 +25,14 @@ with demo:
       with gr.Column():
         inp = gr.Textbox(label="Input", placeholder="Your text")
         upload = gr.File(label="File with text", file_types=[".pdf"])
+        model_selector = gr.Dropdown(choices=models.keys())
         submit = gr.Button("Summarize!")
       with gr.Column():
         with gr.Tab("Text output"):
           out = gr.Textbox(label="Summary", placeholder="Summary will be here", lines=20)
         with gr.Tab("Markdown"):
           out_md = gr.Markdown(label="Summary")
-    submit.click(make_summary, inp, outputs=[out, out_md])
+    submit.click(make_summary, inputs=[inp, model_selector], outputs=[out, out_md])
     upload.upload(parse_pdf, inputs=upload, outputs=inp)
 
 demo.launch()
--- summary.py
+++ summary.py
@@ -1,11 +1,42 @@
 from together import Together
 from dotenv import dotenv_values
+from mistralai import Mistral
 
-client = Together(api_key=dotenv_values(".env")['TOGETHER_API_KEY'])
+client_together = Together(api_key=dotenv_values(".env")['TOGETHER_API_KEY'])
+client_mistral = Mistral(api_key=dotenv_values(".env")['MISTRAL_API_KEY'])
 
-def summarize_req(prompt, text):
-  stream = client.chat.completions.create(
-    model="deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free", #later add selection or test other models
+def summarize_mistral(prompt, text, model):
+  stream = client_mistral.chat.stream(
+    model=model,
+    messages=[
+          {
+            "role": "user",
+            "content": [
+                  {
+                      "type": "text",
+                      "text": prompt,
+                  },
+                  {
+                      "type": "text",
+                      "text": text,
+                  },
+                ]
+          }
+      ],
+    stream=True,
+  )
+  output = ""
+
+  for chunk in stream:
+    output += chunk.data.choices[0].delta.content
+    print(chunk.data.choices[0].delta.content, end="", flush=True)
+  print()
+  if output.startswith('<think>'): output = output.split('</think>')[1]
+  return output.strip()
+
+def summarize_together(prompt, text, model):
+  stream = client_together.chat.completions.create(
+    model=model,
     messages=[
           {
             "role": "user",
@@ -28,8 +59,21 @@ def summarize_req(prompt, text):
   for chunk in stream:
     output += chunk.choices[0].delta.content
     print(chunk.choices[0].delta.content, end="", flush=True)
+  print()
+  if output.startswith('<think>'): output = output.split('</think>')[1]
+  return output.strip()
 
-  return output.split('</think>')[1].strip()
+def summarize_req(prompt, text, model):
+  output = ""
+  print("Using model", model)
+  if model in ["mistral-medium-2505", "magistral-medium-2506"]:
+    output = summarize_mistral(prompt, text, model)
+  elif model in ["meta-llama/Llama-3.3-70B-Instruct-Turbo-Free", "deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free"]:
+    output = summarize_together(prompt, text, model)
+  else:
+    output = "Unknown model"
+  return output
+
 
 def split_text(text, n):
     for start in range(0, len(text), n):
@@ -41,24 +85,25 @@ def split_text(text, n):
             s = start + n
         yield text[start:start+s]
 
-def summarize(input):
+def summarize(input, model):
   max_text_len = 6144
   output = ""
   prompt = open('prompt.txt').read()
+  print("LENGTH", len(input))
   
   if len(input) > max_text_len:
     text_parts = list(split_text(input, max_text_len))  
     prompt = open('prompt_long.txt').read()
 
-    last_summary = summarize_req(prompt, text_parts[0])
+    last_summary = summarize_req(prompt, text_parts[0], model)
     output = last_summary
     
     i = 1
     for text_part in text_parts[1:]:
-        last_summary = summarize_req(prompt, f"EXISTING_SUMMARY: {last_summary}\nNEW_TEXT_CHUNK: {text_part}")
+        last_summary = summarize_req(prompt, f"EXISTING_SUMMARY: {last_summary}\nNEW_TEXT_CHUNK: {text_part}", model)
         output = last_summary
         i += 1
         
   else:
-    output = summarize_req(prompt, f"INPUT_TEXT: {input}")
+    output = summarize_req(prompt, f"INPUT_TEXT: {input}", model)
   return output
