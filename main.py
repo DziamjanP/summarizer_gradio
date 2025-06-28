4f1f646f980db4cd3e9476933ea76ab7c4050dc9
DziamjanP <demyan.postnikov@gmail.com>
2025-06-29 02:07:46 +0300

    add comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import gradio as gr
 
 demo = gr.Blocks(title="Summary maker", fill_height=True)
 
+# Model aliases for dropdown and their names in APIs
 models = {
   "Mistral Medium": "mistral-medium-2505",
   "Magistral Medium": "magistral-medium-2506",
@@ -22,18 +23,22 @@ with demo:
     Paste your text below to get a brief summary
     """)
     with gr.Row(equal_height=False):
+      
       with gr.Column():
         inp = gr.Textbox(label="Input", placeholder="Your text")
         upload = gr.File(label="File with text", file_types=[".pdf"])
         with gr.Accordion("LLM selection", open=False):
           model_selector = gr.Dropdown(choices=models.keys())
         submit = gr.Button("Summarize!")
+
       with gr.Column():
         with gr.Tab("Text output"):
           out = gr.Textbox(label="Summary", placeholder="Summary will be here", lines=20)
         with gr.Tab("Markdown"):
           out_md = gr.Markdown(label="Summary")
+
     submit.click(make_summary, inputs=[inp, model_selector], outputs=[out, out_md])
+    
     upload.upload(parse_pdf, inputs=upload, outputs=inp)
 
 demo.launch()
--- summary.py
+++ summary.py
@@ -5,6 +5,7 @@ from mistralai import Mistral
 client_together = Together(api_key=dotenv_values(".env")['TOGETHER_API_KEY'])
 client_mistral = Mistral(api_key=dotenv_values(".env")['MISTRAL_API_KEY'])
 
+# summarize via mistral.ai API
 def summarize_mistral(prompt, text, model):
   stream = client_mistral.chat.stream(
     model=model,
@@ -27,13 +28,16 @@ def summarize_mistral(prompt, text, model):
   )
   output = ""
 
+  # takes output in chunks, while model thinks
   for chunk in stream:
     output += chunk.data.choices[0].delta.content
-    print(chunk.data.choices[0].delta.content, end="", flush=True)
-  print()
-  if output.startswith('<think>'): output = output.split('</think>')[1]
+    print(chunk.data.choices[0].delta.content, end="", flush=True) # print output for debug
+  print() # newline after printing
+
+  if output.startswith('<think>'): output = output.split('</think>')[1] # remove thinking in present
   return output.strip()
 
+# summarize via together.ai API
 def summarize_together(prompt, text, model):
   stream = client_together.chat.completions.create(
     model=model,
@@ -58,9 +62,10 @@ def summarize_together(prompt, text, model):
 
   for chunk in stream:
     output += chunk.choices[0].delta.content
-    print(chunk.choices[0].delta.content, end="", flush=True)
+    print(chunk.choices[0].delta.content, end="", flush=True) # 
   print()
-  if output.startswith('<think>'): output = output.split('</think>')[1]
+
+  if output.startswith('<think>'): output = output.split('</think>')[1] # Reasoning models use <think/> to show thinking process
   return output.strip()
 
 def summarize_req(prompt, text, model):
@@ -74,30 +79,32 @@ def summarize_req(prompt, text, model):
     output = "Unknown model"
   return output
 
-
+# splits text into chunks of n by finding closest \n or space
 def split_text(text, n):
     for start in range(0, len(text), n):
         t = text[start:start+n]
         s = t.rfind('\n')
         if (start + n - s > n / 2):
-            s = t.rfind(' ')
+            s = t.rfind(' ') # starting from the end so it won't reach the limit
         else :
             s = start + n
         yield text[start:start+s]
 
 def summarize(input, model):
-  max_text_len = 6144
+  max_text_len = 6144 # beacause 8194 is limit for together.ai and 2k for prompt and previous summary
   output = ""
   prompt = open('prompt.txt').read()
-  print("LENGTH", len(input))
+  print("LENGTH", len(input)) # for debug
   
   if len(input) > max_text_len:
     text_parts = list(split_text(input, max_text_len))  
     prompt = open('prompt_long.txt').read()
 
+    # make first summary 
     last_summary = summarize_req(prompt, text_parts[0], model)
     output = last_summary
     
+    # updating summary with new batches
     i = 1
     for text_part in text_parts[1:]:
         last_summary = summarize_req(prompt, f"EXISTING_SUMMARY: {last_summary}\nNEW_TEXT_CHUNK: {text_part}", model)
