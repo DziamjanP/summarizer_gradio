9fda66eb8960f014f7e991cced77a738b5c9241d
DziamjanP <demyan.postnikov@gmail.com>
2025-06-28 21:24:27 +0300

    fix gradio heights add placeholder to summary add constant lines to

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from summary import summarize
 from parser import parse_pdf
 import gradio as gr
 
-demo = gr.Blocks(title="Summary maker")
+demo = gr.Blocks(title="Summary maker", fill_height=True)
 
 def make_summary(input):
   output = summarize(input)
@@ -14,14 +14,14 @@ with demo:
     # Summarizer
     Paste your text below to get a brief summary
     """)
-    with gr.Row(equal_height=True):
+    with gr.Row(equal_height=False):
       with gr.Column():
         inp = gr.Textbox(label="Input", placeholder="Your text")
         upload = gr.File(label="File with text", file_types=[".pdf"])
         submit = gr.Button("Summarize!")
       with gr.Column():
         with gr.Tab("Text output"):
-          out = gr.Textbox(label="Summary")
+          out = gr.Textbox(label="Summary", placeholder="Summary will be here", lines=20)
         with gr.Tab("Markdown"):
           out_md = gr.Markdown(label="Summary")
     submit.click(make_summary, inp, outputs=[out, out_md])
